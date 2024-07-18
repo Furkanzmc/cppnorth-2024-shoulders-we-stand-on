164e1ef77dce387840a9f4496f490ce7383afae0
Furkan Uzumcu <furkanuzumcu@gmail.com>
2024-07-17 20:41:11 -0400

    Update

--- scripts/convert-to-gexf.py
+++ scripts/convert-to-gexf.py
@@ -3,6 +3,12 @@ import networkx as nx
 import argparse
 import sys
 
+COLOR_MAP = {
+    "person": {"r": 255, "g": 0, "b": 0},
+    "product": {"r": 255, "g": 0, "b": 255},
+    "technology": {"r": 200, "g": 100, "b": 0},
+}
+
 
 def yaml_to_gexf(yaml_data, output_file):
     G = nx.DiGraph()
@@ -10,6 +16,14 @@ def yaml_to_gexf(yaml_data, output_file):
     for act, people in yaml_data["Acts"].items():
         for person in people:
             G.add_node(person["name"], act=act)
+            node_color = COLOR_MAP.get(person.get("type", "person"))
+            G.nodes[person["name"]]["viz"] = {
+                "color": {
+                    "r": node_color["r"],
+                    "g": node_color["g"],
+                    "b": node_color["b"],
+                }
+            }
 
             for influence in person.setdefault("influences", []):
                 G.add_node(influence["name"], act=act)
